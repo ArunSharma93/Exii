72e82108a9a1a96e1e4e37b73bbd36bbf6e3ccc9
Arun Sharma <Arun@ArunSharmasMBP.home>
2017-02-04 12:40:38 +0000

    Templates

---
+++ Exii_project/Exii_project/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from rango import views
+
+urlpatterns = [
+    url(r'^', include('rango.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    ]
