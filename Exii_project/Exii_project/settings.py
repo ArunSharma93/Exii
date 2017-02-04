c5e8a86cab7ab551b3923a18bd0e71c6b7434b1a
Arun Sharma <Arun@ArunSharmasMBP.home>
2017-02-04 14:39:59 +0000

    Adding new template folders

--- Exii_project/Exii_project/settings.py
+++ Exii_project/Exii_project/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+SETTINGS_DIR = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -55,7 +62,7 @@ ROOT_URLCONF = 'Exii_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_PATH],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
